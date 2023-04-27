1c171b1c746540c3bf08bfe4ebe3d8be22584b40
ian.dover <ian.dover@skunova.com>
2023-04-27 18:51:35 +0000

    finished problem 3

--- problem3/Q3.py
+++ problem3/Q3.py
@@ -55,14 +55,84 @@ if __name__ == '__main__':
     current_path = os.path.abspath(__file__)
     data_path = os.path.abspath(os.path.join(current_path, '..', '..', 'data', 'ratings.csv'))
 
-    # Part 1
+    # # Part 1
+    # zero_rates = np.arange(0.05, 0.55, 0.05)
+    # reconstruction_errors = []
+    # for zero_rate in zero_rates:
+    #     R = np.genfromtxt(data_path, delimiter = ',')
+    #     R0 = R.copy()
+
+    #     A = np.random.rand(*R.shape) >= (1 - zero_rate)
+    #     R[A] = 0
+    #     m = np.sum(np.sum(R == 0))
+
+    #     Z = PFBS(R0, R, A, m, *R.shape)
+
+    #     reconstruction_errors.append(relative_reconstruction_error(Z, R0))
+
+    #     if zero_rate == 0.5:
+    #         print('#####################################################')
+    #         print('The matrix with missing values:')
+    #         unique_values, counts = np.unique(R.astype(int).flatten(), return_counts = True)
+    #         for value, count in zip(unique_values, counts):
+    #             print(f'{value} appears {count} times.')
+    #         print('#####################################################')
+    #         print('The recovered matrix:')
+    #         unique_values, counts = np.unique(Z.flatten(), return_counts = True)
+    #         for value, count in zip(unique_values, counts):
+    #             print(f'{value} appears {count} times.')
+    #         print('#####################################################')
+    #         print('The original matrix:')
+    #         unique_values, counts = np.unique(R0.astype(int).flatten(), return_counts = True)
+    #         for value, count in zip(unique_values, counts):
+    #             print(f'{value} appears {count} times.')
+    #         print('#####################################################')
+
+    # # ===================================
+    # # Plot 1: Part 1 - Reconstruction Error VS. Percentage of Missing Data
+    # # ===================================
+    # percent_of_missing_data = zero_rates.reshape(-1, 1) * 100
+    # reconstruction_errors = np.array(reconstruction_errors).reshape(-1, 1)
+
+    # plot_np = np.hstack((percent_of_missing_data, reconstruction_errors))
+    # plot_df = pd.DataFrame(plot_np, columns = ['Percent of Missing Data', 'Reconstruction Error'])
+
+    # sns.lineplot(data = plot_df, x = 'Percent of Missing Data', y = 'Reconstruction Error')
+    # plt.title('Part 1: Percent of Missing Data VS. Relative Reconstruction Error')
+    # plt.xlabel('Percent of Missing Data (%)')
+    # plt.ylabel('Relative Reconstruction Error')
+
+    # image_path = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'q3_part1_missing_data_vs_reconstruction_error.png'))
+    # plt.savefig(image_path)
+
+    # plt.clf()
+    # plt.cla()
+    # # ===================================
+
+    # Part 2
     zero_rates = np.arange(0.05, 0.55, 0.05)
     reconstruction_errors = []
     for zero_rate in zero_rates:
         R = np.genfromtxt(data_path, delimiter = ',')
         R0 = R.copy()
 
-        A = np.random.rand(*R.shape) >= (1 - zero_rate)
+        unique_values, counts = np.unique(np.sort(R0.astype(int).flatten()), return_counts = True)
+        count_12 = counts[0] + counts[1]
+        count_34 = counts[2] + counts[3]
+        counts_12345 = sum(counts)
+
+        prob_X = zero_rate * counts_12345 / (3 * count_12 + count_34)
+
+        A = np.zeros(R0.shape, dtype = bool)
+
+        mask_12 = np.random.rand(*R0.shape)
+        condition = ((R0 == 1) | (R0 == 2))
+        A[condition & (mask_12 >= 1 - 3 * prob_X)] = True
+
+        mask_34 = np.random.rand(*R0.shape)
+        condition = ((R0 == 3) | (R0 == 4))
+        A[condition & (mask_34 >= 1 - prob_X)] = True
+
         R[A] = 0
         m = np.sum(np.sum(R == 0))
 
@@ -89,7 +159,7 @@ if __name__ == '__main__':
             print('#####################################################')
 
     # ===================================
-    # Plot 1: Part 1 - Reconstruction Error VS. Percentage of Missing Data
+    # Plot 2: Part 2 - Reconstruction Error VS. Percentage of Missing Data
     # ===================================
     percent_of_missing_data = zero_rates.reshape(-1, 1) * 100
     reconstruction_errors = np.array(reconstruction_errors).reshape(-1, 1)
@@ -98,11 +168,11 @@ if __name__ == '__main__':
     plot_df = pd.DataFrame(plot_np, columns = ['Percent of Missing Data', 'Reconstruction Error'])
 
     sns.lineplot(data = plot_df, x = 'Percent of Missing Data', y = 'Reconstruction Error')
-    plt.title('Part 1: Percent of Missing Data VS. Relative Reconstruction Error')
+    plt.title('Part 2: Percent of Missing Data VS. Relative Reconstruction Error')
     plt.xlabel('Percent of Missing Data (%)')
     plt.ylabel('Relative Reconstruction Error')
 
-    image_path = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'q3_part1_missing_data_vs_reconstruction_error.png'))
+    image_path = os.path.abspath(os.path.join(current_path, '..', '..', 'output', 'q3_part2_missing_data_vs_reconstruction_error.png'))
     plt.savefig(image_path)
 
     plt.clf()
